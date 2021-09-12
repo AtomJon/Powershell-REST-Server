26505992852b6af5d5dfeef97e7b2439ed385391
AtomJon <jonbruskin@gmail,com>
2021-09-12 08:07:04 +0200

    Added resource sub folder

--- handler/SubScriptHandler.go
+++ handler/SubScriptHandler.go
@@ -1,60 +1,21 @@
 package handler
 
 import (
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
-	"os"
-	"path/filepath"
 )
-
-type _ResourceNotFoundError struct {
-	ResourceName string
-}
-
-func (err _ResourceNotFoundError) Error() string {
-	return err.ResourceName;
-}
 
 type SubScriptHandler struct {
 	http.Handler
 }
 
 func (SubScriptHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	reqHandler := _RequestHandler{w, *request, _FindResource}
+	reqHandler := RequestHandler{w, *request, func(s string) (Resource, error) {return Resource{}, nil}, _ExecuteContent}
 	reqHandler.Handle()
 }
 
-func _FindResource(resourceName string) ([]byte, error) {
-	resourcePath := filepath.Join("./routes/", resourceName);
 
-	file, err := os.OpenFile(resourcePath, os.O_RDONLY, 0);
-	if (err != nil) {
-		if (errors.Is(err, os.ErrNotExist)) {
-			return nil, _ResourceNotFoundError{fmt.Sprintf("Could not find resource %s\n", resourcePath)};
-		} else {
-			return _ReturnErrorWithString("Cannot open resource '%s':\n%e\n", resourcePath, err);
-		}
-	}
 
-	stat, err := file.Stat();
-	if (err != nil) {
-		return _ReturnErrorWithString("Cannot get status of resource '%s':\n%v\n", resourcePath, err);
-	}
+func _ExecuteContent(resource Resource) (error, string) {
 
-	if (stat.IsDir()) {
-		return _ReturnErrorWithString("Resource '%s' is dir", resourcePath);
-	}
-
-	content, err := io.ReadAll(file);
-	if (err != nil) {
-		return _ReturnErrorWithString("Cannot read resource '%s':\n%v\n", resourcePath, err);
-	}
-
-	return content, nil
-}
-
-func _ReturnErrorWithString(format string, a ...interface{}) ([]byte, error) {
-	return nil, fmt.Errorf(format, a...);
+	return nil, "Success";
 }
