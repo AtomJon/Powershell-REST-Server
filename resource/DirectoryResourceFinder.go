491ea58b08c86deeaaba6fbb76c4059e3e0a4f8e
AtomJon <jonbruskin@gmail.com>
2021-11-08 17:54:53 +0100

    Added additional resource modifications

--- resource/DirectoryResourceFinder.go
+++ resource/DirectoryResourceFinder.go
@@ -13,7 +13,49 @@ type DirectoryResourceFinder struct {
 	Folder string
 }
 
+func (finder DirectoryResourceFinder) CreateOrModifyResource(resourceName string, contents []byte) (error) {
+	path, err := finder.FindResourcePath(resourceName)
+
+	if err != nil {
+		path = finder.Folder + "/" + resourceName
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(contents)
+	return err
+}
+
 func (finder DirectoryResourceFinder) FindResource(resourceName string) (Resource, error) {
+	resourcePath, err := finder.FindResourcePath(resourceName)
+
+	if err != nil {
+		return Resource{}, err
+	}
+
+	content, err := os.ReadFile(resourcePath)
+	if err != nil {
+		return _ReturnErrorWithString("cannot read resource '%s':\n%v\n", resourcePath, err)
+	}
+
+	return Resource{string(content), filepath.Ext(resourcePath)}, nil
+}
+
+func (finder DirectoryResourceFinder) DeleteResource(resourceName string) (error) {
+	resourcePath, err := finder.FindResourcePath(resourceName)
+
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(resourcePath)
+	return err
+}
+
+func (finder DirectoryResourceFinder) FindResourcePath(resourceName string) (string, error) {
 	resourcePath := finder.Folder + "/**/" + resourceName[1:]
 
 	if len(filepath.Ext(resourcePath)) < 1 {
@@ -22,26 +64,21 @@ func (finder DirectoryResourceFinder) FindResource(resourceName string) (Resourc
 
 	matches, err := zglob.Glob(resourcePath)
 	if err != nil {
-		return _ReturnErrorWithString("invalid pattern '%s':\n%e\n", resourcePath, err)
+		return "", fmt.Errorf("invalid pattern '%s':\n%e", resourcePath, err)
 	}
 
 	if len(matches) < 1 {
-		return Resource{}, ResourceNotFoundError{fmt.Sprintf("could not find resource %s\n", resourcePath)}
+		return "", ResourceNotFoundError{fmt.Sprintf("could not find resource %s\n", resourcePath)}
 	}
 
 	if len(matches) > 1 {
 		reply := fmt.Sprintf("resource is not unique, specify between resources: %s", strings.Join(matches, ", "))
-		return Resource{}, ResourceNotUniqueError{reply}
+		return "", ResourceNotUniqueError{reply}
 	}
 
 	resourcePath = matches[0]
 
-	content, err := os.ReadFile(resourcePath)
-	if err != nil {
-		return _ReturnErrorWithString("cannot read resource '%s':\n%v\n", resourcePath, err)
-	}
-
-	return Resource{string(content), filepath.Ext(resourcePath)}, nil
+	return resourcePath, nil
 }
 
 func _ReturnErrorWithString(format string, a ...interface{}) (Resource, error) {
--- resource/ResourceFinder.go
+++ resource/ResourceFinder.go
@@ -1,5 +1,7 @@
 package resource
 
 type ResourceFinder interface {
+	CreateOrModifyResource(string, []byte) error
 	FindResource(string) (Resource, error)
+	DeleteResource(string) error
 }
